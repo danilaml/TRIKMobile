cacc4ea9ea7ad45715db9d599db8c944e8ba1584
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2015-03-25 21:14:05 +0300

    Updates work.

--- scriptgenerator.cpp
+++ scriptgenerator.cpp
@@ -21,9 +21,12 @@ void ScriptGenerator::handleSend(const QVariant &scriptList) {
     //qDebug() << scriptList.toStringList();
     for (const QString &names : scriptList.toStringList()) {
         qDebug() << scripts.value(names);
-//		Connector connector = new Connector(ipKey);
-//		connector.uploadProgram("ProgramName", names);
-//		connector.runProgram("ProgramName");
+		const QString &names2 = "var __interpretation_started_timestamp__;var pi = 3.14159265;var main = function(){__interpretation_started_timestamp__ = Date.now();brick.display().setBackground(\"white\");while (true) {if (brick.sensor(A1).read() > 50) {brick.sadSmile();} else {brick.smile();}script.wait(10);}}";
+		Connector connector("192.168.1.1");
+		connector.stopRobot();
+		connector.uploadProgram("ProgramName", names2);
+		connector.runProgram("ProgramName");
+		connector.stopRobot();
     }
 }
 
